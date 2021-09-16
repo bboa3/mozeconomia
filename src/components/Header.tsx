e88b83c070e1199b53d618bdcc5c8d9ce7600f46
bboa3 <arlindojosboa@gmail.com>
2021-09-16 11:48:36 +0200

    🏛️ NEW: Linear Chart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import LogoImage from '../assets/images/logo'
 
 const Header: React.FC = () => {
   return (
-    <nav className="w-full">
+    <nav className="w-full flex items-center border-b-2 border-gray-700">
+      <div className="w-7, h-7">
+        <LogoImage />
+      </div>
       <ul className="w-full h-14 flex items-center justify-center">
         <li className="px-5">
           <Link href=""><a className="text-white ">Cambio</a></Link>
